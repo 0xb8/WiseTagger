f9d25bd7ce64c363193ce191dff9146acb4a7b98
cat <cat@wolfgirl.org>
2014-06-25 16:35:10 +0600

    Forgot license headers... again

--- reverse_search.cpp
+++ reverse_search.cpp
@@ -1,5 +1,11 @@
-#include "reverse_search.h"
+/* Copyright © 2014 cat <cat@wolfgirl.org>
+ * This program is free software. It comes without any warranty, to the extent
+ * permitted by applicable law. You can redistribute it and/or modify it under
+ * the terms of the Do What The Fuck You Want To Public License, Version 2, as
+ * published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+ */
 
+#include "reverse_search.h"
 #include <QDesktopServices>
 #include <QStandardPaths>
 #include <QMimeDatabase>
